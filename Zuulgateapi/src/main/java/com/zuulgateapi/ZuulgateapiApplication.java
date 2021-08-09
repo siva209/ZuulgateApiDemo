4b69adbae6f1e6ad96a2df00b03270f2824df15a
siva209 <sivanarayana209@gmail.com>
2021-08-09 14:15:49 +0530

    Added:Zuulgateapi service demo

---
+++ Zuulgateapi/src/main/java/com/zuulgateapi/ZuulgateapiApplication.java
@@ -0,0 +1,17 @@
+package com.zuulgateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulgateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgateapiApplication.class, args);
+		System.out.println("apigateway");
+	}
+
+}
